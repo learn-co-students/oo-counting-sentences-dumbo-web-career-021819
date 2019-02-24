7d5251b70444f4a08e6263d7d442e33cba6ef35d
mnkillebr <killebrew.biz@gmail.com>
2019-02-23 22:31:44 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+    count = 0
+    array.each do |element|
+      if element.length > 2
+        count += 1
+      end
+    end
+    count
   end
 end
